07367e3635028207e632024545fb8130edb013c2
Nitish M <mashalnitish@gmail.com>
2024-07-24 10:13:55 +0530

    new

--- javascript/SpreadOperator/spread.js
+++ javascript/SpreadOperator/spread.js
@@ -1,22 +1,21 @@
-var obj1={
-    name:"Virat",
-    age:35,
-    city:"Delhi"
+var obj1 = {
+    name: "Virat",
+    age: 35,
+    city: "Delhi"
 }
 console.log(obj1)
 
-var obj2={
+var obj2 = {
     ...obj1,
-    id:18,
-    gender:"Male"
+    id: 18,
+    gender: "Male"
 }
 console.log(obj2)
 
-
 //Arrays
 
-var arr=[10,20,30,40]
+var arr = [10, 20, 30, 40]
 console.log(arr)
 
-var arr1=[...arr,50,60,70,80,90]
+var arr1 = [...arr, 50, 60, 70, 80, 90]
 console.log(arr1)
